51c21a0265db4ccd45e7f5c429e7c52d64141ccd
cyberdeuck <60197460+cyberdeuck@users.noreply.github.com>
2023-06-08 11:21:32 -0700

    5 files modified, 4 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,16 +1,14 @@
 // import { Component } from '@angular/core';
-import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
-
-  sharess() {
-    window.alert('The product has been shared!');
-  }
+  @Output() notify = new EventEmitter();
+  // products = [...products];
 }
